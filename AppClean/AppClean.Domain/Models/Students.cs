5efb301707a9a922cdf7a3b73d49d2dde46589b3
Tim <Tim@TIM-PC>
2019-08-15 14:11:19 +0200

    Second commit

---
+++ AppClean/AppClean.Domain/Models/Students.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppClean.Domain.Models
+{
+    public class Students
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ AppClean/AppClean.Infra.Data/Context/UniversityDBContext.cs
@@ -0,0 +1,14 @@
+﻿using AppClean.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppClean.Infra.Data.Context
+{
+    public class UniversityDBContext:DbContext
+    {
+        public UniversityDBContext (DbContextOptions options) : base(options) { }
+        public DbSet<Students> Students { get; set; }
+    }
+}
